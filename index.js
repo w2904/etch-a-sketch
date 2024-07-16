54d4aadd86c8feef5327d9da0e959ef9e7899bc4
w2904 <153371554+w2904@users.noreply.github.com>
2024-07-16 01:03:08 +0000

    Add drawing, color randomizing and reset

--- index.js
+++ index.js
@@ -36,14 +36,36 @@ const drawingInputs = document.querySelector("#drawing-inputs");
 drawingInputs.addEventListener("click", (event) => {
     const target = event.target;
     switch(target.id) {
-        case "start-drawing":
-            localStorage.setItem("drawing", "true");
+        case "blue-button":
+            localStorage.setItem("drawing", "blue");
+            location.reload();
             break;
-        case "stop-drawing":
-            localStorage.setItem("drawing", "false");
+        case "random-button":
+            localStorage.setItem("drawing", "random");
+            location.reload();
             break;
-        case "reset":
+        case "reset-button":
             localStorage.setItem("drawing", "reset");
+            location.reload();
             break;
     }
 });
+
+if (localStorage.getItem("drawing") === "blue" || localStorage.getItem("drawing") === "random") {
+    const grid = document.querySelectorAll(".grid");
+    grid.forEach(square => square.addEventListener("mouseenter", () => {
+        square.classList.replace("grid", "grid-colored");
+        if (localStorage.getItem("drawing") === "random") {
+            square.style.backgroundColor = getRandomColor();
+        }
+    }));
+}
+
+function getRandomColor() {
+    const symbols = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += symbols[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
